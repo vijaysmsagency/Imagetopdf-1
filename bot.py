2620714b810794eeeb0ea12c2e458bb1038e3ddf
Vijay Rai <smswithagency@gmail.com>
2026-04-26 15:47:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     # Replace 'YOUR_TOKEN_HERE' with your actual BotFather token
-    app = ApplicationBuilder().token("8703013141:AAE3_d-pjw5FHZksNHk37Z2yVZdRHEedAGM").build()
+    app = ApplicationBuilder().token("8486316792:AAGN8fOjW5mLrWjjgIWLfrVn7yrRvJVAKME").build()
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
